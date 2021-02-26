98c3bab4c46853f2a19416ca67d8ee9f09c159fb
nj-rubino <njrubino3@gmail.com>
2021-02-26 09:34:15 -0700

    setup more button links and pages

--- start/routes.js
+++ start/routes.js
@@ -18,5 +18,14 @@ const Route = use('Route')
 
 Route.on('/').render('welcome')
 
-Route.on('/future-shows').render('futureshows')
+Route.on('/future-shows').render('future-shows')
 Route.post('/future-shows', 'SeatingChartController.create')
+
+Route.on('/previous-shows').render('previous-shows')
+Route.post('/previous-shows', 'SeatingChartController.create')
+
+Route.on('/add-show').render('add-show')
+Route.post('/add-show', 'SeatingChartController.create')
+
+Route.on('/print-tickets').render('print-tickets')
+Route.post('/print-tickets', 'SeatingChartController.create')
